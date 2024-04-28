5199c184f70e035d3f9a4f511a03d4b7144f5812
Manabu Takahashi <93030854+manaboru@users.noreply.github.com>
2024-04-29 01:32:15 +0900

    Update BaseController.php

--- BaseController.php
+++ BaseController.php
@@ -6,15 +6,11 @@ class BaseController{
 	protected $category;
 	protected $type;
 	protected $action;
-	protected $next_type;
-	protected $next_action;
 	protected $file;
 	protected $form;
-	//protected $renderer;
 	protected $auth;
 	protected $is_system=false;
 	protected $view;
-	protected $title;
 	protected $message;
 	protected $auth_error_mess;
 	protected $login_state;
@@ -33,19 +29,12 @@ public function set_system($flag){
 private function view_initialize(){
 	//画面表示クラス
 	$this->view = new Smarty;
+	
 	//Smarty関連ディレクトリの設定
 	$this->view->template_dir	=_SMARTY_TEMPLATES_DIR;
 	$this->view->compile_dir	=_SMARTY_TEMPLATES_C_DIR;
 	$this->view->config_dir		=_SMARTY_CONFIG_DIR;
 	$this->view->cache_dir		=_SMARTY_CACHE_DIR;
-
-	//入力チェック用フラグ
-	//$this->form = new HTML_QuickForm();
-	//JavaScriptのメッセージを日本語に修正
-	//$this->formsetJsWarnings("入力エラーです。","上記項目を修正してください")；
-
-	//HTML_QickFormとSmartyを使うためのクラス
-	//$this->rernderer = new HTML_QuickForm_Renderer_ArraySmarty($this->view);
 
 	//リクエスト変数　typeとactionで動作を決める
 	if(isset($_REQUEST['category'])){
@@ -69,25 +58,11 @@ private function view_initialize(){
 //フォームと変数を読み込んでテンプレートに組み込んで表示します。
 //-------------------------------------------------------------
 protected function view_display(){
-	//セッション変数などの内容の表示
-//	$this->debug_display();//このメソッドは、このファイルにこの後追加するものなので、とりあえずキャンセル
-
-//	$this->view->assign('lastname',$_SESSION[_MEMBER_AUTHINFO]['lastname']);
-//	$this->view->assign('firstname',$_SESSION[_MEMBER_AUTHINFO]['firstname']);
-
 	//ログイン状況の表示
 	$this->disp_login_state();
-
-//	$this->view->assign('title',$this->title);
 	$this->view->assign('auth_error_mess', $this->auth_error_mess);
-//	$this->view->assign('message',$this->message);
 	$this->view->assign('disp_login_state',$this->login_state);
-//	$this->view->assign('category',$this->next_category);
-//	$this->view->assign('type',$this->next_type);
-//	$this->view->assign('action',$this->next_action);
 	$this->view->assign('debug_str',$this->debug_str);
-	//$this->form->accept($this->renderer);
-//	$this->view->assign('form',$this->renderer->toArray());
 	$this->view->display($this->file);
 }
 
@@ -106,50 +81,6 @@ private function disp_login_state(){
 	$this->login_state="ログイン処理が不備";
 	}
 }
-
-//----------------------------------------------------
-// 検索処理関係
-//----------------------------------------------------
-//
-// pageIDをURLに追加。
-//
-public function add_pageID(){
-	if( $this->type !== 'list' ){ return;}
-
-	$add_pageID = "";
-		if(isset($_GET['pageID']) && $_GET['pageID'] != ""){
-		$add_pageID = '&pageID=' . $_GET['pageID'];
-		$_SESSION['pageID'] = $_GET['pageID'];
-		}else if(isset($_SESSION['pageID']) && $_SESSION['pageID'] != ""){
-		$add_pageID = '&pageID=' . $_SESSION['pageID'];
-		}
-	return $add_pageID;
-}
-
-//-------------------------------------------------------------
-//　ページネーション
-//-------------------------------------------------------------
-public function make_page_link($data){
-
-//require_once 'Jumping.php';//これ、これだよここ。Xamppでは既にインストールされているので、インクルードファイルから既に読み込み済みだからredeclareになってしまう。fatal Error候補。
-
-	$params = array(
-	            'mode'      => 'Jumping',
-	            'perPage'   => 10,
-	            'delta'     => 10,
-	            'itemData'  => $data
-	        );
-
-	$pager = new Pager_Jumping($params);
-        $data  = $pager->getPageData();
-        $links = $pager->getLinks();
-
-return array($data, $links);
-
-//return $data;
-}
-
-
 
 
 }//----class終了

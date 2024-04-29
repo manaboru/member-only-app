907d1e45f574edd5e46a05fa48dc7d42f0fa4323
Manabu Takahashi <93030854+manaboru@users.noreply.github.com>
2024-04-29 11:19:40 +0900

    Update view_object.php

--- view_object.php
+++ view_object.php
@@ -2,26 +2,11 @@
 
 class view_object extends BaseController{
 
-  //***************************************************
-  //	コンストラクタ
-  //***************************************************
-//public function __construct(){
-
-	//データベースを操作します。
-//	$this->systemModel = new systemModel();
-//	$this->articleModel = new articleModel();
-//	$this->editorialModel = new editorialModel();
-//	$this->convert = new convertValues();
-
-
-//}//---コンストラクタ終了
-
 public function screen($arr,$file){
 
 	//ログインユーザー名の表示
 	$this->view->assign('lastname',$_SESSION[_MEMBER_AUTHINFO]['lastname']);
 	$this->view->assign('firstname',$_SESSION[_MEMBER_AUTHINFO]['firstname']);
-
 	
 	//引数を繰り返しassign
 	foreach($arr as $key =>$value){
